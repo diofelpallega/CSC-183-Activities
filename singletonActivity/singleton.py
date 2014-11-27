d110647d59d7eadbe69456c93e074ba3a454fbab
supersherlock <diofel.pallega@gmail.com>
2014-11-27 21:06:32 +0800

    [EDITED](singleton.py) added client code

--- singletonActivity/singleton.py
+++ singletonActivity/singleton.py
@@ -40,7 +40,7 @@ def traverse_site(max_links=10):
         except Exception:
             continue    
 
-        # Skip if not a web page
+    # Skip if not a web page
         if status.get('content-type') != 'text/html':
             continue
         # Add the link to queue for downloading images
@@ -60,7 +60,7 @@ def traverse_site(max_links=10):
             if parsed.netloc and parsed.netloc != parsed_root.netloc:
                 continue
             # Construct a full url from a link which can be relative
-            link_url = (parsed.scheme or parsed_root.scheme) + '://'  + \
+            link_url = (parsed.scheme or parsed_root.scheme) + '://' + /
             (parsed.netloc or parsed_root.netloc) + parsed.path or ''
     
             # If link was added previously, skip it
@@ -103,23 +103,3 @@ def download_images(thread_name):
                 urllib.urlretrieve(src, os.path.join('images', basename))
 
         print thread_name, 'finished downloading images from', url
-
-if __name__ == '__main__':
-    root = 'http://python.org'
-    parsed_root = urlparse(root)
-    singleton = Singleton()
-    singleton.queue_to_parse = [root]
-    # A set of urls to download images from
-    singleton.to_visit = set()
-    # Downloaded images
-    singleton.downloaded = set()
-    traverse_site()
-    # Create images directory if not exists
-    if not os.path.exists('images'):
-        os.makedirs('images')
-    # Create new threads
-    thread1 = ImageDownloaderThread(1, "Thread-1", 1)
-    thread2 = ImageDownloaderThread(2, "Thread-2", 2)
-    # Start new Threads
-    thread1.start()
-    thread2.start()

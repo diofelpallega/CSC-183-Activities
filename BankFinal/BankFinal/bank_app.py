8c896190c25ad0e76bb9d8eb8092178d2fb3d292
supersherlock <diofel.pallega@gmail.com>
2014-11-30 01:35:48 +0800

    [DONE][CPROFILE1]

--- BankFinal/BankFinal/bank_app.py
+++ BankFinal/BankFinal/bank_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from bank.account import Account
 from bank.bank import Bank
 
 app = Flask(__name__)
@@ -11,7 +12,11 @@ def hello_world():
     return render_template('index.html', balance=balance)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import cProfile
+   
+    account = Account('1111', 50)
+    BANK.add_account(account)
+    cProfile.run('app.run(debug=True)')
 
 
 

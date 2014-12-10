989813951da59f3e8294a7975eff24a23efd88a2
supersherlock <diofel.pallega@gmail.com>
2014-12-10 18:44:11 +0800

    [Added CdRentalFinal]

---
+++ cdrentalFinal/test/unit/cd_test.py
@@ -0,0 +1,16 @@
+﻿
+import unittest
+from cd.cd import *
+
+class TestCustomer(unittest.TestCase):
+    def test_if_customer_account_can_be_created(self):
+        cd = Cd("01","hlo","false","jack")
+        self.assertEqual(cd.cd_id, "01")
+        self.assertEqual(cd.title, "hlo")
+        self.assertEqual(cd.isrented, "false")
+        self.assertEqual(cd.rentedBy,"jack")
+
+
+
+if __name__ == '__maine__':
+    unittest.main()

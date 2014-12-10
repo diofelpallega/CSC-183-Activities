9201c38729f2064d0de6064b49d916839ca56296
supersherlock <diofel.pallega@gmail.com>
2014-12-10 18:43:48 +0800

    [removed cdrentalfinal]

--- CdRentalFinal/cd/cd.py
+++
@@ -1,8 +0,0 @@
-class Cd(object):
-	def __init__(self, cd_id, title, isrented, rentedBy):
-		self.cd_id = cd_id
-		self.title = title
-		self.isrented = isrented
-		self.rentedBy = rentedBy
-
- 
--- CdRentalFinal/cd/customer.py
+++
@@ -1,8 +0,0 @@
-class Customer(object):
-	def __init__(self, customer_id, name, rented, rentedArray):
-		self.customer_id = customer_id
-		self.name = name
-		self.rented = rented
-		self.rentedArray = rentedArray
-
- 
